7901d5290f1b6ba579f7719e19700d32e33b30fd
biglongbeach <biglongbeach@gmail.com>
2015-10-23 20:41:57 -0400

    kodi installer for ubuntu

---
+++ kodi-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "install kodi"
+
+echo "sudo apt-get install software-properties-common"
+sudo apt-get install software-properties-common
+echo complete && echo continue && echo sleep 3
+sleep 3
+
+echo "sudo add-apt-repository ppa:team-xbmc/ppa"
+sudo add-apt-repository ppa:team-xbmc/ppa
+echo complete && echo continue && echo sleep 3
+sleep 3
+
+echo "sudo apt-get update"
+sudo apt-get update
+echo complete && echo continue && echo sleep 3
+sleep 3
+
+echo "sudo apt-get install kodi"
+sudo apt-get install kodi
+echo complete && echo continue && echo sleep 3
+sleep 3
