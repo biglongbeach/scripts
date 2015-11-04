54273f6750a48a6161a76b6e69fe3a8816667cc9
biglongbeach <biglongbeach@gmail.com>
2015-11-03 23:01:15 -0500

    added a libary install

--- kodi-install.sh
+++ kodi-install.sh
@@ -20,4 +20,10 @@ sleep 3
 echo "sudo apt-get install kodi"
 sudo apt-get install kodi
 echo complete && echo continue && echo sleep 3
+
+echo install libraries to get sportdevil to work
+sudo apt-get install librtmp1
+echo complete && echo continue && echo sleep 3
+sleep 3
+
 sleep 3
